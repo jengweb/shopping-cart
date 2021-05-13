32a9e3c0e3c2f611be5ce5902ec4756b7a08e864
jengweb <jengweb@hotmail.com>
2021-05-13 15:12:36 +0700

    [Updated]: Test Payment UpdateStock Return Errors

--- store-service/internal/payment/payment_test.go
+++ store-service/internal/payment/payment_test.go
@@ -143,66 +143,41 @@ func Test_ConfirmPayment_Input_OrderID_8004359103_And_PaymentDetail_Should_Be_Ge
 	assert.Equal(t, errors.New("GetOrderProduct Error"), err)
 }
 
-// func Test_ConfirmPayment_Input_OrderID_8004359103_And_PaymentDetail_Should_Be_GetOrderProduct_Return_Errors(t *testing.T) {
-// 	expectedMessage := ""
+func Test_ConfirmPayment_Input_OrderID_8004359103_And_PaymentDetail_Should_Be_UpdateStock_Return_Errors(t *testing.T) {
+	expectedMessage := ""
 
-// 	orderId := 8004359103
-// 	paymentDetail := payment.PaymentDetail{
-// 		CardNumber:   "4719700591590995",
-// 		CVV:          "752",
-// 		ExpiredMonth: 7,
-// 		ExpiredYear:  20,
-// 		CardName:     "Karnwat Wongudom",
-// 		TotalPrice:   104.95,
-// 		MerchantID:   154124000,
-// 	}
-// 	shippingInfo := order.ShippingInfo{
-// 		ShippingMethod:       "Kerry",
-// 		ShippingAddress:      "405/35 ถ.มหิดล",
-// 		ShippingSubDistrict:  "ท่าศาลา",
-// 		ShippingDistrict:     "เมือง",
-// 		ShippingProvince:     "เชียงใหม่",
-// 		ShippingZipCode:      "50000",
-// 		RecipientName:        "ณัฐญา ชุติบุตร",
-// 		RecipientPhoneNumber: "0970804292",
-// 	}
+	orderId := 8004359103
+	paymentDetail := payment.PaymentDetail{
+		CardNumber:   "4719700591590995",
+		CVV:          "752",
+		ExpiredMonth: 7,
+		ExpiredYear:  20,
+		CardName:     "Karnwat Wongudom",
+		TotalPrice:   104.95,
+		MerchantID:   154124000,
+	}
 
-// 	mockBankGateway := new(mockBankGateway)
-// 	mockBankGateway.On("Payment", paymentDetail).Return("TOY202002021525", nil)
+	mockBankGateway := new(mockBankGateway)
+	mockBankGateway.On("Payment", paymentDetail).Return("TOY202002021525", nil)
 
-// 	mockShippingGateway := new(mockShippingGateway)
-// 	mockShippingGateway.On("ShipByKerry", shippingInfo).Return("1785261900", nil)
+	mockOrderRepository := new(mockOrderRepository)
+	mockOrderRepository.On("GetOrderProduct", orderId).Return([]order.OrderProduct{
+		{
+			ProductID: 2,
+			Quantity:  1,
+		},
+	}, nil)
 
-// 	mockOrderRepository := new(mockOrderRepository)
-// 	mockOrderRepository.On("GetOrderProduct", orderId).Return([]order.OrderProduct{
-// 		{
-// 			ProductID: 2,
-// 			Quantity:  1,
-// 		},
-// 	}, nil)
-// 	mockOrderRepository.On("UpdateOrder", orderId, "TOY202002021525").Return(nil)
+	mockProductRepository := new(mockProductRepository)
+	mockProductRepository.On("UpdateStock", 2, 1).Return(errors.New("UpdateStock Error"))
 
-// 	mockProductRepository := new(mockProductRepository)
-// 	mockProductRepository.On("UpdateStock", 2, 1).Return(nil)
+	paymentService := payment.PaymentService{
+		BankGateway:       mockBankGateway,
+		OrderRepository:   mockOrderRepository,
+		ProductRepository: mockProductRepository,
+	}
 
-// 	mockShippingRepository := new(mockShippingRepository)
-// 	mockShippingRepository.On("GetShippingByOrderID", orderId).Return(shippingInfo, nil)
-
-// 	fixedTime, _ := time.Parse("2/1/2006T15:04:05", "1/3/2020T13:30:00")
-
-// 	paymentService := payment.PaymentService{
-// 		BankGateway:        mockBankGateway,
-// 		ShippingGateway:    mockShippingGateway,
-// 		OrderRepository:    mockOrderRepository,
-// 		ProductRepository:  mockProductRepository,
-// 		ShippingRepository: mockShippingRepository,
-// 		Time: func() time.Time {
-// 			return fixedTime
-// 		},
-// 	}
-
-// 	actualMessage, err := paymentService.ConfirmPayment(orderId, paymentDetail)
-// 	assert.Equal(t, expectedMessage, actualMessage)
-// 	assert.Equal(t, nil, err)
-
-// }
+	actualMessage, err := paymentService.ConfirmPayment(orderId, paymentDetail)
+	assert.Equal(t, expectedMessage, actualMessage)
+	assert.Equal(t, errors.New("UpdateStock Error"), err)
+}
